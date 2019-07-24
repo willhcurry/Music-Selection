c1960ef793a06e23ab22c799572c9900265cf007
willhcurry <willhcurry@gmail.com>
2019-07-24 02:03:59 -0600

    Added Song Detail

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 const App = () => {
     return (
@@ -7,7 +8,10 @@ const App = () => {
             <div className="ui row">
                 <div className="column eight wide">
                     <SongList />
-                </div>  
+                </div>
+                <div className="column eight wide">
+                    <SongDetail />
+                </div>
             </div>   
         </div>
     );
